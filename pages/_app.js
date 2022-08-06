0c607f89925a67efcbf953e1a0e54c44787ac91e
Rafli Ghani <ghanirafli8@gmail.com>
2022-08-07 00:35:28 +0700

    favicon.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,14 @@
 import Head from 'next/head'
 import '../styles/globals.css'
+import favicon from '../public/favicon.ico'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+      <Head>
+        <link rel="icon" href={favicon.src} />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
