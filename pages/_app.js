170a272ea970a61c99b115485e5363ace260a5b1
Rafli Ghani <ghanirafli8@gmail.com>
2022-08-07 00:28:58 +0700

    favico

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,7 @@ import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <>
-      <Head>
-        <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico"/>
-    </Head>
-    <Component {...pageProps} />
-  </>
+  return <Component {...pageProps} />
 }
 
 export default MyApp
